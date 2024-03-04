64d4dd32d3b09a9ca3ea6507d6c801e35195c97c
«My <myEmail@example.com>
2024-03-04 16:13:51 +0500

    add blog

---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,5 @@
+<h1>My Second Blog Post</h1>
+<p class="lorem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quas quos animi voluptas inventore cumque non veritatis. Doloribus ex repellendus adipisci quae eius et eos. Ab debitis rerum quisquam esse.
+</p>
+<p class="lorem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quas quos animi voluptas inventore cumque non veritatis. Doloribus ex repellendus adipisci quae eius et eos. Ab debitis rerum quisquam esse.
+</p>
