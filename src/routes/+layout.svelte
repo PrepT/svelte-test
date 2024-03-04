44cebbbb3e19e1f79e8bda5502fa3b6d28816627
«My <myEmail@example.com>
2024-03-04 14:42:28 +0500

    add main page and nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to HomePage">Имя Фамилия</a>
+        <div class="nav-links">
+            <a href="/about" class="link">Обо мне</a>
+            <a href="/blog" class="link">Блог</a>
+            <a href="/project" class="link">Портфолио</a>
+            <a href="/contact" class="link">Контакты</a>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <!--страницы укажем ниже-->
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50 px auto;
+    }
+</style>
